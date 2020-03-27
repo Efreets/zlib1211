b857f9439abd8abe4c8cb8412eff6bba0a6323a9
Efreet <Efreet@AVALON.local>
2020-03-27 12:59:06 +0800

    Update CMakeCache.txt, CMakeCCompiler.cmake, and 431 more files...

---
+++ Build/CMakeFiles/zlib.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/adler32.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/adler32.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/compress.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/compress.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/crc32.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/crc32.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/deflate.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/deflate.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/gzclose.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/gzclose.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/gzlib.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/gzlib.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/gzread.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/gzread.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/gzwrite.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/gzwrite.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/infback.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/infback.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/inffast.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/inffast.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/inflate.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/inflate.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/inftrees.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/inftrees.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/trees.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/trees.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/uncompr.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/uncompr.obj"
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11/zutil.c" "M:/gcc930/zlib-1.2.11/Build/CMakeFiles/zlib.dir/zutil.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ZLIB_DLL"
+  "_LARGEFILE64_SOURCE=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "M:/gcc930/zlib-1.2.11/zlib-1.2.11"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: M:/gcc930/zlib-1.2.11/zlib-1.2.11
+# Build directory: M:/gcc930/zlib-1.2.11/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "M:/gcc930/zlib-1.2.11/zlib-1.2.11/CMakeLists.txt;235;add_test;M:/gcc930/zlib-1.2.11/zlib-1.2.11/CMakeLists.txt;0;")
+add_test(example64 "example64")
+set_tests_properties(example64 PROPERTIES  _BACKTRACE_TRIPLES "M:/gcc930/zlib-1.2.11/zlib-1.2.11/CMakeLists.txt;244;add_test;M:/gcc930/zlib-1.2.11/zlib-1.2.11/CMakeLists.txt;0;")
